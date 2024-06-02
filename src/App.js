bd77bd6529f04bc47874d2a67e26c8db8f406eb2
Basudev2806 <basudev.kar.kolkata@gmail.com>
2024-06-03 01:35:31 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <CurrentLocation />
       </div>
       <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+        <a href="https://medium.com/@basudev2806kar/create-weather-app-using-react-js-with-current-location-search-city-e3f5a3e3af6e">
           Download Source Code
         </a>{" "}
         | Developed by{" "}
