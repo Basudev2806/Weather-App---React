25c586f173d2a4097f43a28fd02c2a5f9cfbb981
Basudev2806 <basudev.kar.kolkata@gmail.com>
2024-06-03 00:45:19 +0530

    Weather App

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
+
 module.exports = {
-  key: process.env.API_KEY,
+  key: "002b46e5178c58505a41b01193081e07",
   base: "https://api.openweathermap.org/data/2.5/",
 };
